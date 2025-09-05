f75746e7405dc807bebf013d5dda84ac688fb6a9
Xinxi Qin <xinxi.qin@gmail.com>
2025-09-04 23:07:25 -0400

    add pics

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>3239 Legends Preserve Dr, Daytona Beach, FL 32124</h1>
+        <img src="./pic1_3239.webp" width="806" height="604" alt="pic1" />
+        <div style={{ display: "flex", gap: "0px" }}>
+          <img src="./pic2_3239.webp" width="403" height="302" alt="pic2" />
+          <img src="./pic3_3239.webp" width="403" height="302" alt="pic3" />
+        </div>
+        <img src="./pic4_3239.webp" width="806" height="604" alt="pic4" />
+        <div style={{ display: "flex", gap: "0px" }}>
+          <img src="./pic5_3239.webp" width="403" height="302" alt="pic5" />
+          <img src="./pic6_3239.webp" width="403" height="302" alt="pic6" />
+        </div>
+        <img src="./pic7_3239.webp" width="806" height="604" alt="pic7" />
+        <img src="./pic8_3239.webp" width="806" height="604" alt="pic8" />
+
+
+      </header>      
+    </div>
+  );
+}
+
+export default App;
+
+
+
